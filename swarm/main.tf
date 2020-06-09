028f23ea693a21f2613c68318ac0fcf55ade404f
Matt Short <mattshort77@gmail.com>
2020-06-09 10:05:24 +0100

    adding subnet in

--- swarm/main.tf
+++ swarm/main.tf
@@ -9,3 +9,13 @@ resource "aws_vpc" "swarm-vpc" {
     Name = "swarm-vpc"
   }
 }
+
+#subnet
+resource "aws_subnet" "swarm-subnet" {
+  vpc_id     = "${aws_vpc.swarm-vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "swarm-subnet"
+  }
+}
