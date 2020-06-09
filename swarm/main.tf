64b78c2bb12684af971bd0dc1551a0ef01d7123f
Matt Short <mattshort77@gmail.com>
2020-06-09 10:05:59 +0100

    updating reference for v12

--- swarm/main.tf
+++ swarm/main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "swarm-vpc" {
 
 #subnet
 resource "aws_subnet" "swarm-subnet" {
-  vpc_id     = "${aws_vpc.swarm-vpc.id}"
+  vpc_id     = aws_vpc.swarm-vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
